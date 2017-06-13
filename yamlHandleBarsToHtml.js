eb190a63f3cf2e80fe429a9f488533c2ab4fa474
SinbadFreedom <151229152@qq.com>
2017-06-14 01:31:32 +0800

    add markdown support

--- yamlHandleBarsToHtml.js
+++ yamlHandleBarsToHtml.js
@@ -2,6 +2,9 @@ var Handlebars = require("handlebars");
 var yaml = require('js-yaml');
 var fs = require('fs');
 
+var showdown = require('showdown'),
+    converter = new showdown.Converter();
+
 /** Check for arguments*/
 if (process.argv.length < 4) {
     console.log("Usage: node yaml2html.js mustache_file_name yaml_file_name out_html_name");
@@ -21,6 +24,13 @@ fs.readFile(mustacheFile, 'utf8', function (error, mustache_data) {
     /** Get document, or throw exception on error */
     try {
         var yaml_data = yaml.safeLoad(fs.readFileSync(yamlFile, 'utf8'));
+        /** markdown convert helper*/
+        Handlebars.registerHelper('markdown', function (fileName) {
+            var md_data = fs.readFileSync(fileName, 'utf8');
+            var html = converter.makeHtml(md_data);
+            return html;
+        });
+
         var htmlContent = compiled(yaml_data);
         /** output html*/
         fs.writeFile(outHtmlFile, htmlContent, function () {
